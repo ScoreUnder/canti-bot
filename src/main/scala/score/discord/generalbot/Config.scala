ded1475d53f1e668eb9437e982bba77cdce73de4
Score_Under <seejay.11@gmail.com>
2017-07-12 18:03:11 +0100

    Remove now-unused BOT_OWNER constant in favour of configuration

--- src/main/scala/score/discord/generalbot/Config.scala
+++ src/main/scala/score/discord/generalbot/Config.scala
@@ -6,8 +6,6 @@ import score.discord.generalbot.wrappers.jda.ID
 class Config(val token: String, val owner: ID[User])
 
 object Config {
-  val BOT_OWNER = new ID[User](226521865537978368L)  // TODO: Make this configurable
-
   def load(config: com.typesafe.config.Config) = {
     new Config(
       token = config.getString("token"),
