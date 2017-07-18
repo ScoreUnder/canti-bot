616ffe2482979b501f792b302d7462528b65c7c8
Score_Under <seejay.11@gmail.com>
2017-07-18 20:20:34 +0100

    Add blame command
    Shows who is responsible for making the bot say a given message.

---
+++ src/main/scala/score/discord/generalbot/command/BlameCommand.scala
@@ -0,0 +1,41 @@
+package score.discord.generalbot.command
+
+import net.dv8tion.jda.core.entities.Message
+import score.discord.generalbot.functionality.Commands
+import score.discord.generalbot.functionality.ownership.MessageOwnership
+import score.discord.generalbot.util.{APIHelper, BotMessages}
+import score.discord.generalbot.wrappers.FutureEither._
+import score.discord.generalbot.wrappers.jda.Conversions._
+import score.discord.generalbot.wrappers.jda.ID
+
+import scala.async.Async._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.Try
+
+class BlameCommand(commands: Commands)(implicit messageOwnership: MessageOwnership) extends Command.Anyone {
+  override def name = "blame"
+
+  override def aliases = Nil
+
+  override def description = "Reveal the user who caused the bot to send a given message"
+
+  override val longDescription =
+    s"""Usage: `${commands.prefix}$name id` where `id` is the message ID to look up.
+       |To get the ID, turn on Developer Mode, right click the message, and press "Copy ID".
+     """.stripMargin
+
+  override def execute(message: Message, args: String): Unit = {
+    async {
+      val resultText = await(for {
+        id <- Future.successful(
+          Try(ID fromString args).fold(_ => Left("Expecting a message ID; got something else"), Right(_))
+        ).flatView
+        owner <- messageOwnership(message.getJDA, id).map(_.toRight("No ownership info available for that message")).flatView
+      } yield s"That message is owned by ${owner.mention} (${owner.name}).")
+      message.getChannel.sendOwned(
+        resultText.fold(BotMessages.error, BotMessages.plain),
+        owner = message.getAuthor)
+    }.failed.map(APIHelper.loudFailure("getting a message's blame", message.getChannel))
+  }
+}
--- src/main/scala/score/discord/generalbot/wrappers/jda/ID.scala
+++ src/main/scala/score/discord/generalbot/wrappers/jda/ID.scala
@@ -3,3 +3,11 @@ package score.discord.generalbot.wrappers.jda
 import slick.lifted.MappedTo
 
 final class ID[+T](val value: Long) extends AnyVal with MappedTo[Long]
+
+object ID {
+  def fromString[T](string: String) = new ID[T](try {
+    java.lang.Long.parseUnsignedLong(string)
+  } catch {
+    case _: NumberFormatException => string.toLong
+  })
+}
