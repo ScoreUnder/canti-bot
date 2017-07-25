f262a8d04f061a6f05c46e722e28b630be287a44
Score_Under <seejay.11@gmail.com>
2017-07-25 18:35:31 +0100

    Give IDs a toString
    This was missed when restructuring the Command.OneUserOnly class to take
    an ID instead of a Long for the user ID.

--- src/main/scala/score/discord/generalbot/wrappers/jda/ID.scala
+++ src/main/scala/score/discord/generalbot/wrappers/jda/ID.scala
@@ -2,7 +2,9 @@ package score.discord.generalbot.wrappers.jda
 
 import slick.lifted.MappedTo
 
-final class ID[+T](val value: Long) extends AnyVal with MappedTo[Long]
+final class ID[+T](val value: Long) extends AnyVal with MappedTo[Long] {
+  override def toString = value.toString
+}
 
 object ID {
   def fromString[T](string: String) = new ID[T](try {
