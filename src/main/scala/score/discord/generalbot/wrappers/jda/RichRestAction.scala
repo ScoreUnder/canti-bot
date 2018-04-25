71eb140c07b17f7035a980fc28335513dfc7ef0d
Score_Under <seejay.11@gmail.com>
2018-04-25 05:36:53 +0100

    Add type annotation

--- src/main/scala/score/discord/generalbot/wrappers/jda/RichRestAction.scala
+++ src/main/scala/score/discord/generalbot/wrappers/jda/RichRestAction.scala
@@ -2,10 +2,10 @@ package score.discord.generalbot.wrappers.jda
 
 import net.dv8tion.jda.core.requests.RestAction
 
-import scala.concurrent.Promise
+import scala.concurrent.{Future, Promise}
 
 class RichRestAction[T](val orig: RestAction[T]) extends AnyVal {
-  def queueFuture() = {
+  def queueFuture(): Future[T] = {
     val promise = Promise[T]()
     orig.queue({ result => promise success result }, { result => promise failure result })
     promise.future
