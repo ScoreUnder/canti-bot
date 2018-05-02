3a14cb3fa8a5ba770d6d03e9b1eebc0718b2450e
Score_Under <seejay.11@gmail.com>
2018-05-02 21:10:18 +0100

    Escape single tildes when escaping formatting
    Single tildes don't format anything, but the previous approach leaves us
    vulnerable to escaping only one tilde of a row of 3, leaving a double
    tilde still "active".

--- src/main/scala/score/discord/generalbot/util/MessageUtils.scala
+++ src/main/scala/score/discord/generalbot/util/MessageUtils.scala
@@ -4,7 +4,7 @@ object MessageUtils {
   def blockMentionsNaive(message: String): String =
     message.replace("@", "@\u200C")
 
-  private val FORMAT_MATCHER = s"(?<!\\\\)(\\*|_|~~|`)".r
+  private val FORMAT_MATCHER = s"(?<!\\\\)([*_~`])".r
 
   def escapeFormatting(message: String): String =
     FORMAT_MATCHER.replaceAllIn(message, "\\\\$1")
