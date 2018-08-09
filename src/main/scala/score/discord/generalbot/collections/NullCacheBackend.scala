3a9be1d18514489f2530d440dc62b59608a4d7fe
Score_Under <seejay.11@gmail.com>
2018-08-10 00:27:48 +0100

    Make NullCacheBackend more IDE-friendly

--- src/main/scala/score/discord/generalbot/collections/NullCacheBackend.scala
+++ src/main/scala/score/discord/generalbot/collections/NullCacheBackend.scala
@@ -4,7 +4,7 @@ import scala.concurrent.Future
 import scala.language.existentials
 
 object NullCacheBackend {
-  def of[K](cache: Cache[K, Option[V]] forSome {type V}) = new cache.Backend[K] {
+  def of[K, V](cache: Cache[K, Option[V]]) = new cache.Backend[K] {
     override def keyToId(key: K): K = key
 
     override def missing(key: K): Future[Option[Nothing]] = Future.successful(None)
