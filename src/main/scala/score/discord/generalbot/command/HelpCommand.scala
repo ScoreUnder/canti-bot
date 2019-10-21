bb6352d688108149a87c66eddb98a0312ca0f157
Score_Under <seejay.11@gmail.com>
2019-10-21 15:30:01 +0100

    Remove unnecessary async from HelpCommand

--- src/main/scala/score/discord/generalbot/command/HelpCommand.scala
+++ src/main/scala/score/discord/generalbot/command/HelpCommand.scala
@@ -8,7 +8,6 @@ import score.discord.generalbot.functionality.ownership.MessageOwnership
 import score.discord.generalbot.util.BotMessages
 import score.discord.generalbot.wrappers.jda.Conversions._
 
-import scala.async.Async._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.Try
@@ -23,12 +22,12 @@ class HelpCommand(commands: Commands)(implicit val messageOwnership: MessageOwne
   override def description = "Show descriptions for all commands, or view one command in detail"
 
   override def executeAndGetMessage(message: Message, args: String): Future[Message] =
-    async {
+    Future {
       ((args match {
         case "" => Some(1)
         case x => Try(x.toInt).toOption
       }) match {
-        case Some(page) => await(showHelpPage(message, page))
+        case Some(page) => showHelpPage(message, page)
         case None => showCommandHelp(args)
       }).fold(BotMessages.error, identity).toMessage
     }
@@ -46,27 +45,25 @@ class HelpCommand(commands: Commands)(implicit val messageOwnership: MessageOwne
   }
 
   private def showHelpPage(message: Message, page: Int) = {
-    async {
-      val myCommands = commands.all.filter(_ checkPermission message)
-      val pageOffset = pageSize * (page - 1)
-      val numPages = (myCommands.length + pageSize - 1) / pageSize
+    val myCommands = commands.all.filter(_ checkPermission message)
+    val pageOffset = pageSize * (page - 1)
+    val numPages = (myCommands.length + pageSize - 1) / pageSize
 
-      if (page < 1)
-        Left("ಠ_ಠ")
-      else if (page > numPages)
-        Left(s"There are only $numPages pages, but you asked for page $page. That page does not exist.")
-      else {
-        val helpList = myCommands.slice(pageOffset, pageOffset + pageSize)
-        val embed = new EmbedBuilder()
-        embed.appendDescription("You can erase most replies this bot sends to you by reacting with ❌ or 🚮.\n" +
-          s"**Commands (page $page of $numPages):**\n")
+    if (page < 1)
+      Left("ಠ_ಠ")
+    else if (page > numPages)
+      Left(s"There are only $numPages pages, but you asked for page $page. That page does not exist.")
+    else {
+      val helpList = myCommands.slice(pageOffset, pageOffset + pageSize)
+      val embed = new EmbedBuilder()
+      embed.appendDescription("You can erase most replies this bot sends to you by reacting with ❌ or 🚮.\n" +
+        s"**Commands (page $page of $numPages):**\n")
 
-        for (command <- helpList) {
-          embed appendDescription s"`${commands.prefix}${command.name}`: ${command.description}\n"
-        }
-
-        Right(embed)
+      for (command <- helpList) {
+        embed appendDescription s"`${commands.prefix}${command.name}`: ${command.description}\n"
       }
+
+      Right(embed)
     }
   }
 }
