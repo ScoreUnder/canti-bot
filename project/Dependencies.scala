d858b793bdf69646e4ab7eeb88eb5052cd98cefa
Score_Under <seejay.11@gmail.com>
2021-11-19 16:29:04 +0000

    We are not quite scala 3 yet, scala-steward.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,9 @@
-import sbt.*
+import sbt._
 
-object Dependencies:
+object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10"
   lazy val jda = ("net.dv8tion" % "JDA" % "4.3.0_346")
     .exclude("net.java.dev.jna", "jna")
     .exclude("club.minnced", "opus-java-api")
     .exclude("club.minnced", "opus-java-natives")
+}
