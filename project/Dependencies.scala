a3d7266a7468dffc7736ecd8c403052d109a12a5
Scala Steward <scala_steward@virtuslab.com>
2023-03-04 21:45:39 +0000

    Update JDA to 4.4.1_353

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
-  lazy val jda = ("net.dv8tion" % "JDA" % "4.4.0_352")
+  lazy val jda = ("net.dv8tion" % "JDA" % "4.4.1_353")
     .exclude("net.java.dev.jna", "jna")
     .exclude("club.minnced", "opus-java-api")
     .exclude("club.minnced", "opus-java-natives")
