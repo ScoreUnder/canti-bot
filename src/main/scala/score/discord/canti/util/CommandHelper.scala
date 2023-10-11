929acfe658c0d8cb26b79e8877717d1a29ed8743
Score_Under <seejay.11@gmail.com>
2023-10-11 17:54:07 +0100

    Reimplement stripMentions

--- src/main/scala/score/discord/canti/util/CommandHelper.scala
+++ src/main/scala/score/discord/canti/util/CommandHelper.scala
@@ -4,10 +4,14 @@ import net.dv8tion.jda.api.entities.{Guild, Member}
 import net.dv8tion.jda.api.{JDA, entities}
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder
 import score.discord.canti.wrappers.NullWrappers.*
-import score.discord.canti.wrappers.jda.RichMessage.guild
 
 object CommandHelper:
-  /** Sanitise all mentions in the provided text for use with Discord.
+  private val mentionsRegex = """<(@&?|#)!?(-?\d+)>""".r
+
+  /** Replace all mentions in the provided text with readable plaintext,
+    * for use outside of Discord.
+    *
+    * Not for sanitising mentions out of messages to be sent to Discord.
     *
     * @param guild
     *   optional guild to use in resolving members/channels/etc
@@ -16,14 +20,18 @@ object CommandHelper:
     * @return
     *   sanitised text
     */
-  def mentionsToPlaintext(guild: Option[Guild], message: String)(using jda: JDA): String = ???
-    /*import net.dv8tion.jda.api.entities.Message.MentionType.*
-    val builder = MessageCreateBuilder().setContent(message).nn
-    guild match
-      case Some(guild) =>
-        builder.stripMentions(guild, USER, ROLE, CHANNEL)
-      case None =>
-        builder.stripMentions(jda, USER, ROLE, CHANNEL)
-    builder.getStringBuilder.nn.toString
-    */
-    // TODO: stripMentions is no longer part of the API
+  def mentionsToPlaintext(guild: Option[Guild], message: String)(using jda: JDA): String =
+    mentionsRegex.replaceSomeIn(message, { m =>
+      val mentionType = m.group(1)
+      for
+        id <- m.group(2).toLongOption
+        plaintext <-
+          mentionType match
+            case "#" => jda.getGuildChannelById(id).?.map(c => s"#${c.getName}")
+            case "@" =>
+              guild.flatMap(_.getMemberById(id).?.map(m => s"@${m.getEffectiveName}"))
+                .orElse(jda.getUserById(id).?.map(u => s"@${u.getName}"))
+            case "@&" => jda.getRoleById(id).?.map(r => s"@${r.getName}")
+            case _ => throw IllegalStateException("Unexpected match in mentions regex")
+      yield plaintext
+    })
