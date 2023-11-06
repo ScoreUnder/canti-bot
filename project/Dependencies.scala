d3f70a8037fb41536c4e7972279f908bcced241a
Scala Steward <scala_steward@virtuslab.com>
2023-11-06 20:45:33 +0000

    Update JDA to 5.0.0-beta.17

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.17"
-  lazy val jda = ("net.dv8tion" % "JDA" % "5.0.0-beta.16")
+  lazy val jda = ("net.dv8tion" % "JDA" % "5.0.0-beta.17")
     .exclude("net.java.dev.jna", "jna")
     .exclude("club.minnced", "opus-java-api")
     .exclude("club.minnced", "opus-java-natives")
